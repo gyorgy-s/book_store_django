4fff691a0a6ad46a8cb6730f4caef818657f3623
gyorgy <none>
2024-01-25 06:56:39 +0100

    feat: add further fields to the existing model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 
 class Book(models.Model):
+    title = models.CharField(max_length=150, null=False, blank=False, default=None)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(limit_value=1), MaxValueValidator(limit_value=5)]
+    )
+    author = models.CharField(max_length=50, null=True)
+    is_bestselling = models.BooleanField(default=False)
 
-    title = models.CharField(max_length=50)
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    def __str__(self):
+        return f"{self.title}"
