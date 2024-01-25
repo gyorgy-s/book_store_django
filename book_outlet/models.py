8f15c0187c7e2657330c236444b0f1ab2498a869
gyorgy <none>
2024-01-25 11:20:24 +0100

    feat: add book detail view

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
 
 # Create your models here.
 
@@ -13,6 +14,9 @@ class Book(models.Model):
     )
     author = models.CharField(max_length=50, null=True)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"book_id": self.id})
 
     def __str__(self):
         return f"{self.title}"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Book
 
@@ -11,4 +11,24 @@ def home(request):
         {
             "books": books,
         },
+    )
+
+
+def book_detail(request, book_id):
+    # try:
+    #     book = Book.objects.get(id=book_id)
+    # except Book.DoesNotExist:
+    #     raise Http404()
+
+    book = get_object_or_404(Book, id=book_id)
+
+    return render(
+        request,
+        "book_outlet/book_detail.html",
+        {
+            "title": book.title,
+            "author": book.author,
+            "rating": book.rating,
+            "is_bestselling": book.is_bestselling,
+        }
     )
