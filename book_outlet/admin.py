66e417d4baf20b6c36b2436792294fb5e792476c
gyorgy <none>
2024-01-27 13:59:54 +0100

    feat: display additional book information on admin site

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -7,6 +7,8 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("id", "title", "author", "rating")
 
 
 admin.site.register(Book, BookAdmin)
