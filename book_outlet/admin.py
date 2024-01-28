dc413e5a7935f0e6685f80888cb905dba992b266
gyorgy <none>
2024-01-28 12:40:19 +0100

    feat: add author model

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Author, Book
 
 # Register your models here.
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("first_name", "last_name")
+    list_display = ("first_name", "last_name")
+
+
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_filter = ("author", "rating",)
+    list_filter = (
+        "author",
+        "rating",
+    )
     list_display = ("id", "title", "author", "rating")
 
 
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -3,7 +3,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
+
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
 
 
 class Book(models.Model):
@@ -13,7 +23,7 @@ class Book(models.Model):
         max_digits=3,
         validators=[MinValueValidator(limit_value=1), MaxValueValidator(limit_value=5)],
     )
-    author = models.CharField(max_length=50, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
 
